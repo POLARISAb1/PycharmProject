d8480f0636f08d338e3f72119d6c848b3f65edd1
POLARISAb1 <m18625572652@163.com>
2022-03-23 13:52:14 +0800

    变更

--- data/demo.py
+++ data/demo.py
@@ -1,8 +1,10 @@
 # coding=utf-8
-import openpyxl
 
 # 获取工作簿
-book = openpyxl.load_workbook('test.xlsx')
+from openpyxl import load_workbook
+
+book = load_workbook('test.xlsx')
+print(book.sheetnames)
 
 # 读取工作表
 sheet = book.active
@@ -12,10 +14,34 @@ cell_a1 = sheet['A1']
 cell_a3 = sheet.cell(column=1, row=3)
 
 # 读取多个连续单元格
-cells = sheet['A':'C']
-# cells = sheet.rows
+# cells = sheet.iter_rows(max_row=3, max_col=2, values_only=True)
+# print(type(cells), cells)
+# print(isinstance(cells, Iterable))
+# for row in cells:
+#     print(type(row), row)
+#     # 继续遍历
+#     for cc in row:
+#         print(type(cc), cc)
 
-print(cells)
+#
+# r = sheet.rows
+# print(r)
+#
+# # for cell in r:
+# #     print(cell)
+# # 创建并返回一个新对象
+# cl = tuple(r)
+# print(cl)
 
 # 获取单元格的值
-print(cell_a1.value)
+
+# a_1 = sheet['A1']
+# print(a_1.value)
+
+
+# all_s = sheet.values
+# print(all_s)
+# for row in all_s:
+#     # print(type(row), row)
+#     for cell in row :
+#         print(cell)
